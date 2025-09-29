cbc8d13245d5156ac8a702954e820141864ef55c
Kavyatotad1406 <totadkavya@gmail.com>
2025-09-29 15:40:40 +0530

    Save

---
+++ swapping.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+print("\nBefore Swapping:")
+print("First number =", num1)
+print("Second number =", num2)
+temp = num1
+num1 = num2
+num2 = temp
+print("\nAfter Swapping:")
+print("First number =", num1)
+print("Second number =", num2)
